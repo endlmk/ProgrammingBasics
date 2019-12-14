e06df1313ff473cddc62536f51c11ea9cd6d1d9d
endlmk <endlmk@users.noreply.github.com>
2019-12-14 22:31:18 +0900

    Done chap.10

--- chap10.ml
+++ chap10.ml
@@ -1,4 +1,5 @@
 open Chap8
+open Chap9
 (* 昇順に並んだ整数リストと整数から整列したリストを返す *)
 (* insert: int list -> int -> int list *)
 let rec insert lst n = match lst with 
@@ -126,3 +127,56 @@ let%test _ = saita_ketsueki [{ name = "bear"; hight = 157.; weight = 65.; birth_
                               { name = "order"; hight = 157.; weight = 65.; birth_month = 1; birth_day = 31; blood = "AB"; };
                                { name = "order"; hight = 157.; weight = 65.; birth_month = 1; birth_day = 31; blood = "AB"; }]
             = "AB"
+
+(* 目的: ２つのリストの長さが同じかどうかを返す *)
+(* equal_length: a' list -> a' list -> bool *)
+let rec equal_length lst1 lst2 = match (lst1, lst2) with
+    ([],[]) -> true
+  | (_,[]) -> false
+  | ([],_) -> false
+  | (_::rest1,_::rest2) -> equal_length rest1 rest2
+
+let%test _ = equal_length [1;2;3] [4;5;6] = true
+let%test _ = equal_length [1;2;3] [4;5] = false
+let%test _ = equal_length [] [4;5] = false
+let%test _ = equal_length ["a"; "b"] [4;5] = true
+
+(* 目的: 駅名リストからその駅の漢字表記を返す *)
+(* romaji_to_kanji: string -> ekimei_t list -> string *)
+let rec romaji_to_kanji romaji lst = match lst with 
+    [] -> ""
+  | {kanji=k;kana=_;romaji=r;shozoku=_}::rest -> if r = romaji 
+                                                    then k 
+                                                    else romaji_to_kanji romaji rest
+
+let%test _ = romaji_to_kanji "myogadani" global_ekimei_list = "茗荷谷"
+let%test _ = romaji_to_kanji "shibuya" global_ekimei_list = "渋谷"
+let%test _ = romaji_to_kanji "kameoka" global_ekimei_list = ""
+
+(* 目的: 漢字の駅名２つからその2駅間の距離を返す *)
+(* get_ekikan_kyori: string -> string -> float *)
+let rec get_ekikan_kyori e1 e2 lst = match lst with
+    [] -> infinity
+  | {kiten=k;shuten=s;keiyu=_;kyori=ky;jikan=_;}::rest -> if (e1=k && e2=s) || (e1=s && e2=k)
+                                                              then ky
+                                                              else get_ekikan_kyori e1 e2 rest
+
+let%test _ = get_ekikan_kyori "茗荷谷" "新大塚" global_ekikan_list = 1.2
+let%test _ = get_ekikan_kyori "三越前" "大手町" global_ekikan_list = 0.7
+let%test _ = get_ekikan_kyori "九段下" "大手町" global_ekikan_list = infinity
+
+(* 目的: ローマ字の駅名2つからその間の距離を調べ、文字列を返す *)
+(* kyori_wo_hyoji: string -> string -> string *)
+let kyori_wo_hyoji e1 e2 = let k1 = romaji_to_kanji e1 global_ekimei_list in
+                              if k1 = "" then e1 ^ "という駅は存在しません" else
+                                let k2 = romaji_to_kanji e2 global_ekimei_list in
+                                  if k2 = "" then e2 ^ "という駅は存在しません" else
+                                    let ky = get_ekikan_kyori k1 k2 global_ekikan_list in
+                                      if ky = infinity then k1 ^ "と" ^ k2 ^ "はつながっていません" else
+                                        k1 ^ "から" ^ k2 ^ "までは" ^ (string_of_float ky) ^ "kmです"
+
+
+let%test _ = kyori_wo_hyoji "myogadani" "shinotsuka" = "茗荷谷から新大塚までは1.2kmです"
+let%test _ = kyori_wo_hyoji "korakuen" "myogadani" = "後楽園から茗荷谷までは1.8kmです"
+let%test _ = kyori_wo_hyoji "kudanshita" "otemachi" = "九段下と大手町はつながっていません"
+let%test _ = kyori_wo_hyoji "korakuen" "kameoka" = "kameokaという駅は存在しません"
